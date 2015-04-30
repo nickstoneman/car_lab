91ace555ba65c221dd17032c751e81fce2a55809
Nick Stoneman <nick.stoneman@gmail.com>
2015-04-30 17:26:28 +0100

    test 5 pass

--- spec/test.js
+++ spec/test.js
@@ -8,7 +8,7 @@ describe('Car', function(){
 // every time this test runs it will use this car object.
   beforeEach(function(){
     // create a new myCar object each time
-    myCar = new Car("Dodge","Charger", 2015, "Red", "off", "", "Manufacturer");
+    myCar = new Car("Dodge","Charger", 2015, "Red", "off", "", "Manufacturer", "");
   });
 
   describe('#year', function(){
@@ -34,9 +34,9 @@ describe('Car', function(){
       expect(myCar.currentOwner).to.equal("Manufacturer");
     });
   });
-
+// deleted the x
   describe('#passengers', function(){
-    xit('should exist and initially be empty', function(){
+    it('should exist and initially be empty', function(){
       expect(myCar.passengers).to.exist.to.be.empty;
     });
   });
